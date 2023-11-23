b52cf1f4ff4e4c2cb749f4859191ebd0b051dffe
Aleksey Kosolapov <klealekle@gmail.com>
2023-11-23 15:22:59 +0300

    gh pages static

---
+++ components/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">index</NuxtLink></li>
+        <li><NuxtLink to="/working">working</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
