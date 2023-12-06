4cb10c26e7561aa3957e632fce5bca7bb06cc9d6
Aleksey Kosolapov <klealekle@gmail.com>
2023-12-06 23:23:02 +0300

    matches rows

--- components/MatchesRow.vue
+++ components/MatchesRow.vue
@@ -1,26 +1,21 @@
 <template>
-  <div class="flex border-dashed odd:bg-gray-800 odd:bg-opacity-10 first:border-0">
+  <div class="flex border-b border-b-slate-800 odd:bg-gray-800 odd:bg-opacity-10 last:border-0 hover:bg-opacity-10 hover:bg-gray-700">
     <div class="flex grow items-center">
-      <div class="flex items-start md:items-center py-1 pl-2 md:pl-4 w-[70px] md:w-36 flex-col md:flex-row">
-        <img class="h-5" src="../assets/img/icons/dota2.svg" alt="gameName" />
-        <div class="md:ml-2">
-          <div class="font-bold flex-col text-sm dark:text-red-500 leading-4 hidden md:flex" v-if="match.isLive">LIVE</div>
-          <span class="bg-red-500 text-gray-900 text-xs font-semibold mt-0.5 px-1 rounded dark:bg-red-500 dark:text-gray-900" v-if="match.isLive">MAP{{ match.map }}</span>
-        </div>
+      <div class="flex items-start md:items-center py-1 pl-2 md:pl-4 w-[70px] md:w-36 flex-col md:flex-row text-secondary">
+        <component :is="match.game" :fontControlled="false" class="w-5" />
       </div>
-      <div class="flex grow py-1 gap-2 md:gap-5 relative px-1 md:px-5">
+      <div class="flex grow gap-2 md:gap-5 relative px-1">
         <div class="flex grow flex-1 justify-end items-end gap-1 md:gap-5 flex-col md:flex-row-reverse md:items-center md:justify-start">
           <a href="#" class="flex h-6 w-6">
             <img class="h-6 max-w-full" :src="match.t1Icon" :alt="match.t1" />
           </a>
           <a href="#" class="flex text-right text-white text-[13px] leading-4"> {{ match.t1 }}</a>
         </div>
-        <router-link to="/" class="flex justify-center px-1 border-x border-gray-800 flex-col w-16 md:w-20 md:h-10 md:px-2">
-          <div class="flex items-center justify-center">
-            
-          </div>
-          <div class="justify-center text-center text-sm font-extrabold">{{ match.score }}</div>
-        </router-link>
+        <div class="flex flex-col justify-center items-center px-1 bg-gray-800 bg-opacity-20 w-16 md:w-20 md:h-14 md:px-2">
+            <div class="badge badge-error badge-outline cursor-pointer" v-if="match.isLive && !isScore" @click="onShowScore">score</div>
+            <div class="text-sm font-bold text-red-400" v-if="match.isLive && isScore" @click="onShowScore">{{ match.score }}</div>
+            <div class="text-sm font-bold" v-if="!match.isLive && !isScore">{{ match.score }}</div>
+        </div>
         <div class="flex grow flex-1 content-center md:items-center text-left gap-1 md:gap-5 flex-col md:flex-row">
           <a href="#" class="flex h-6 w-6">
             <img class="h-6 max-w-full" :src="match.t2Icon" :alt="match.t2" />
@@ -40,7 +35,7 @@
           {{ match.date }}
           {{ match.time }}
         </div>
-        <a href="/" class="tooltip w-7" :data-tip="match.eventName"><img class="" :src="match.eIcon" :alt="match.eventName" /></a>
+        <a href="/" class="tooltip w-7 flex justify-center" :data-tip="match.eventName"><img class="h-5" :src="match.eIcon" :alt="match.eventName" /></a>
       </div>
     </div>
   </div>
@@ -48,7 +43,10 @@
 
 <script setup lang="ts">
 import ClockIcon from "@/assets/img/icons/clock.svg";
-
+const isScore = ref(false)
+const onShowScore = () => {
+    isScore.value = !isScore.value
+}
 interface IMatch {
   id: number;
   t1: string;
