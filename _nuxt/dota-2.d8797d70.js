5a0c0118fcfff14680e88d4aa83e21b759fb0a3f
Aleksey Kosolapov <klealekle@gmail.com>
2023-12-25 10:10:27 +0300

    Updates

---
+++ _nuxt/dota-2.d8797d70.js
@@ -0,0 +1,1 @@
+import{_ as e,v as t,t as c}from"./entry.389a8b0f.js";const o={};function n(r,a){return c(),t("h1",null,"Dota2")}const _=e(o,[["render",n]]);export{_ as default};
--- _nuxt/working.f37faa1a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,v as n,t}from"./entry.5c9fc1f5.js";const c={};function o(r,a){return t(),n("div",null," Page content working.vue ")}const _=e(c,[["render",o]]);export{_ as default};
