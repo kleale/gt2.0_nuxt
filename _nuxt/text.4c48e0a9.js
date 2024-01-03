41629974a8497b4882919f8fd782edc11ce177f6
Aleksey Kosolapov <klealekle@gmail.com>
2024-01-03 16:09:15 +0300

    Updates

---
+++ _nuxt/text.4c48e0a9.js
@@ -0,0 +1,1 @@
+import{_ as e,v as t,t as n}from"./entry.3d79425f.js";const c={};function o(r,a){return n(),t("div",null," Page content working.vue ")}const _=e(c,[["render",o]]);export{_ as default};
